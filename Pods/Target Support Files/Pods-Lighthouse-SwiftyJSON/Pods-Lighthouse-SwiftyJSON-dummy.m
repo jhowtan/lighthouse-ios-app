ddd8c32f86b7a4dd659c1460e22f370c667f85ab
Roland Baldovino <roland.baldovino@digitalartsnetwork.com>
2015-06-29 19:35:29 +0800

    Add missing pods

---
+++ Pods/Target Support Files/Pods-Lighthouse-SwiftyJSON/Pods-Lighthouse-SwiftyJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lighthouse_SwiftyJSON : NSObject
+@end
+@implementation PodsDummy_Pods_Lighthouse_SwiftyJSON
+@end
---
+++ Pods/Target Support Files/Pods-Lighthouse/Pods-Lighthouse-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 3
+
+// EstimoteSDK
+#define COCOAPODS_POD_AVAILABLE_EstimoteSDK
+#define COCOAPODS_VERSION_MAJOR_EstimoteSDK 3
+#define COCOAPODS_VERSION_MINOR_EstimoteSDK 2
+#define COCOAPODS_VERSION_PATCH_EstimoteSDK 5
+
+// Firebase
+#define COCOAPODS_POD_AVAILABLE_Firebase
+#define COCOAPODS_VERSION_MAJOR_Firebase 2
+#define COCOAPODS_VERSION_MINOR_Firebase 3
+#define COCOAPODS_VERSION_PATCH_Firebase 1
+
+// GoogleAPISwiftClient
+#define COCOAPODS_POD_AVAILABLE_GoogleAPISwiftClient
+#define COCOAPODS_VERSION_MAJOR_GoogleAPISwiftClient 0
+#define COCOAPODS_VERSION_MINOR_GoogleAPISwiftClient 1
+#define COCOAPODS_VERSION_PATCH_GoogleAPISwiftClient 0
+
+// GoogleAPISwiftClient/Common
+#define COCOAPODS_POD_AVAILABLE_GoogleAPISwiftClient_Common
+#define COCOAPODS_VERSION_MAJOR_GoogleAPISwiftClient_Common 0
+#define COCOAPODS_VERSION_MINOR_GoogleAPISwiftClient_Common 1
+#define COCOAPODS_VERSION_PATCH_GoogleAPISwiftClient_Common 0
+
+// ObjectMapper
+#define COCOAPODS_POD_AVAILABLE_ObjectMapper
+#define COCOAPODS_VERSION_MAJOR_ObjectMapper 0
+#define COCOAPODS_VERSION_MINOR_ObjectMapper 12
+#define COCOAPODS_VERSION_PATCH_ObjectMapper 0
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 0
+
+// google-plus-ios-sdk
+#define COCOAPODS_POD_AVAILABLE_google_plus_ios_sdk
+#define COCOAPODS_VERSION_MAJOR_google_plus_ios_sdk 1
+#define COCOAPODS_VERSION_MINOR_google_plus_ios_sdk 7
+#define COCOAPODS_VERSION_PATCH_google_plus_ios_sdk 1
+
