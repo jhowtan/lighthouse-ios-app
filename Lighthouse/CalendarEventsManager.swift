14b615b498a0d25c1bce781e55e17d99599b95e6
Jonathan Tan <jonathan.tan@digitalartsnetwork.com>
2015-07-03 10:24:12 +0800

    Test NSDate methods in CalendarEventsManager to prepare for Calendar calls

--- Lighthouse/CalendarEventsManager.swift
+++ Lighthouse/CalendarEventsManager.swift
@@ -7,17 +7,27 @@
 //
 
 import Foundation
+import SwiftDate
 import SwiftyJSON
 import GoogleAPIs
+import Alamofire
 
 class CalendarEventsManager {
     let roomsRef = Firebase(url:"https://beacon-dan.firebaseio.com/rooms/")
     var roomList = [Room]()
     
     // Instantiate the Singleton
-    static let sharedInstance = CalendarEventsManager()
+    class var sharedInstance : CalendarEventsManager {
+        struct Singleton {
+            static let instance = CalendarEventsManager()
+        }
+        return Singleton.instance
+    }
     
-    // -------- FIREBASE METHODS --------------------------
+    // Reference to current TableViewController
+    var currentTableView: ItemsTableViewController?
+
+    // -------- FIREBASE METHODS -----------
     func cacheFirebaseData() {
         roomsRef.observeSingleEventOfType(.Value, withBlock: { room in
             let child = room.children
@@ -26,13 +36,53 @@ class CalendarEventsManager {
                 let json = JSON(r)
                 // Create new beacon object
                 var nRoom = Room(json: json)
-                
+
                 // Push room to roomList
                 self.roomList.append(nRoom)
             }
-            println(self.roomList)
+        })
+        
+        self.getFreeBusy()
+    }
+    
+    // ------ Calendar(Events) Methods -------
+    func getFreeBusy() {
+        let now = NSDate()
+        let later = now.add("minute", value: 30)!
+        println("now: \(now)")
+        println("later: \(later)")
+//        let serviceName = "Calendar"
+//        let apiVersion = "3.0"
+//        let endpoint = "freeBusy"
+//        let params = ["timeMin" : "", "timeMax": "", "items" : [String]() ]
+//        
+   
+    }
+
+    func changeRoomState(mutatedRoom : Room) {
+        // .Value will always be triggered last so the ordering does not matter
+        // We just need to cache the messages on load to pass to the Messages View
+        
+        // This is just used to list the messages the current user has
+        roomsRef.observeEventType(.ChildChanged, withBlock: { changedRoom in
+            let json = JSON(changedRoom.value)
+            var key = json["name"].string
+            var cRoom : Room
+            for r in self.roomList {
+                // Find and retrieve changed room in list
+                if (r.name == key) {
+                    cRoom = r
+                }
+            }
+            // Do something with cRoom
+
         })
     }
     
+    func displayRooms(rooms: Room) {
+        if(currentTableView != nil){
+            currentTableView!.insertNewObject()
+        }
+    }
     
 }
